06e01c06052da3093c76de034c875b713a7e12d8
Jing LUO <jing.luo@team.telstra.com>
2019-11-18 12:37:24 +1100

    Project created.

---
+++ GumtreeCC/GumtreeCC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GumtreeCC
+//
+//  Created by Jing LUO on 18/11/19.
+//  Copyright © 2019 Jing LUO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
